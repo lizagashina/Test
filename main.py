834a2c7edc4dfdc616bedd5c754386de9c9122e9
lizagashina <77lizagashina77@gmail.com>
2024-12-22 19:42:12 +0300

    For example

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
-def print_hi(name):
-    print(f'Hi, {name}')
+def print_hi():
+    heart_rate = 60
+    denominator = input('Введите делитель: ')
+    try:
+        denominator = float(denominator)
+        new_heart_rate = heart_rate / denominator
+        print(f'Новая ЧСС: {new_heart_rate}')
+    except ZeroDivisionError:
+        print('Не делите, пожалуйста, на ноль')
+    except ValueError:
+        print('Используйте только цифры и разделитель "." для ввода')
+
 
 if __name__ == '__main__':
-    print_hi('Liza')
+    data = input()
 
+    list_data = data.split()
+    max_len = 0
+    result = ""
+
+    for word in list_data:
+        if len(word) > max_len:
+            max_len = len(word)
+            result = word
+
+    print(result)
